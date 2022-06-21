47c75d05b43b480a0b5279a4729a1084eb1f4153
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-06-22 01:22:03 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,59 @@
+SOURCE_FILE = ./src/main.rs
+
+release_mac_universal: $(SOURCE_FILE)
+	cargo build --release --target aarch64-apple-darwin
+	cargo build --release --target x86_64-apple-darwin
+	lipo -create -output ./release/kusa ./target/aarch64-apple-darwin/release/kusa ./target/x86_64-apple-darwin/release/kusa
+	tar acvf release/kusa_mac_universal.tar.gz release/kusa
+
+release_linux: $(SOURCE_FILE)
+	cross build --release --target aarch64-unknown-linux-gnu
+	cross build --release --target x86_64-unknown-linux-gnu
+	cp ./target/aarch64-unknown-linux-gnu/release/kusa kusa
+	tar acvf release/kusa_aarch64_linux.tar.gz kusa
+	rm kusa
+	cp ./target/x86_64-unknown-linux-gnu/release/kusa kusa
+	tar acvf release/kusa_x86_64_linux.tar.gz kusa
+	rm kusa
+
+release_windows: $(SOURCE_FILE)
+	cross build --release --target x86_64-pc-windows-gnu
+	cross build --release --target i686-pc-windows-gnu
+	cp ./target/x86_64-pc-windows-gnu/release/kusa.exe kusa.exe
+	zip release/kusa_x86_64-windows.zip kusa.exe
+	rm kusa.exe
+	cp ./target/i686-pc-windows-gnu/release/kusa.exe kusa.exe
+	zip release/kusa_i686-windows.zip kusa.exe
+	rm kusa.exe
+
+.PHONY: release
+release:
+	mkdir -p release
+	make release_mac_universal --no-print-directory
+	make release_linux --no-print-directory
+	make release_windows --no-print-directory
+
+git_add: $(SOURCE_FILE)
+	sed -i -e s/$${GITHUB_ACCESS_TOKEN}/GITHUB_ACCESS_TOKEN/ $(SOURCE_FILE)
+	rm ./src/main.rs-e
+	@if !(cat $(SOURCE_FILE) | grep $${GITHUB_ACCESS_TOKEN}); then \
+		git add .;\
+		echo changed to be staged; \
+	else \
+		echo token is included in the code; \
+	fi
+
+git_push: $(SOURCE_FILE)
+	@if !(cat $(SOURCE_FILE) | grep $${GITHUB_ACCESS_TOKEN}); then \
+		git push origin main; \
+		sed -i -e s/GITHUB_ACCESS_TOKEN/$${GITHUB_ACCESS_TOKEN}/ $(SOURCE_FILE); \
+		rm ./src/main.rs-e; \
+	else \
+		echo token is included in the code; \
+	fi
+
+.PHONY: clean
+clean:
+	rm -rf ./target
+	rm kusa
+	rm *.gz
---
+++ src/main.rs
@@ -0,0 +1,219 @@
+use ::reqwest::blocking::Client;
+use anyhow::{Context, Result};
+use clap::Parser;
+use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
+use std::process;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.graphql",
+    query_path = "query.graphql",
+    response_derives = "Debug"
+)]
+struct Kusa;
+
+type Date = String;
+
+#[derive(Parser)]
+#[clap(
+    name = "kusa",
+    version = "0.1.0",
+    about = "Command to get a chart of the number of Github contributions",
+)]
+struct Command {
+    #[clap(name = "github user name", action = clap::ArgAction::Set)]
+    user_name: String,
+}
+
+struct RGB {
+    r: i64,
+    g: i64,
+    b: i64,
+}
+
+struct DailyStatus {
+    date: String,
+    contribution_count: i64,
+    color: String,
+}
+
+impl DailyStatus {
+    fn get_month(&self) -> usize {
+        self.date
+            .split("-")
+            .collect::<Vec<_>>()[1]
+            .parse()
+            .unwrap()
+    }
+}
+
+trait HexToRGB {
+    fn get_rgb(&mut self) -> RGB;
+}
+
+impl HexToRGB for String {
+    fn get_rgb(&mut self) -> RGB {
+        self.remove(0);//#ebedf0 -> ebedf0
+        let v = i64::from_str_radix(&*self, 16).unwrap() as f64;
+        RGB {
+            r: (v / 256_f64.powf(2.0) % 256.0) as i64,
+            g: (v / 256_f64.powf(1.0) % 256.0) as i64,
+            b: (v / 256_f64.powf(0.0) % 256.0) as i64,
+        }
+    }
+}
+
+fn get_github_contributions(response_data: kusa::ResponseData) -> (i64, Vec<Vec<DailyStatus>>) {
+    match response_data.user {
+        Some(user) => {
+            let contribution_calendar = user
+                .contributions_collection
+                .contribution_calendar;
+
+            let total_contributions = contribution_calendar.total_contributions;
+
+            let weekly_status =
+                contribution_calendar.weeks
+                    .iter()
+                    .map(|weekly_status| {
+                        weekly_status.contribution_days
+                            .iter()
+                            .map(|daily_status| {
+                                DailyStatus {
+                                    date: daily_status.date.to_string(),
+                                    contribution_count: daily_status.contribution_count,
+                                    color: daily_status.color.to_string(),
+                                }
+                            })
+                            .collect()
+                    })
+                    .collect();
+            return (total_contributions, weekly_status);
+        }
+        None => {
+            println!("No users found");
+            process::exit(1)
+        }
+    }
+}
+
+fn post_graphql_query(user_name: &str) -> Result<kusa::ResponseData> {
+    let github_access_token = "GITHUB_ACCESS_TOKEN";
+
+    let variables = kusa::Variables {
+        user_name: user_name.to_string(),
+    };
+
+    let client = Client::builder()
+        .user_agent("graphql-rust/0.10.0")
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str(&format!("Bearer {}", github_access_token))
+                    .unwrap(),
+            ))
+                .collect(),
+        )
+        .build()?;
+
+    let response_body =
+        post_graphql::<Kusa, _>(&client, "https://api.github.com/graphql", variables)?;
+
+    response_body.data.context("failed to fetch data")
+}
+
+fn transpose(weekly_statuses: &Vec<Vec<DailyStatus>>) -> Vec<Vec<&DailyStatus>> {
+    let week_count = weekly_statuses.len();
+    let mut kusa: Vec<Vec<&DailyStatus>> = Vec::new();
+    for column_index in 0..7 {
+        let mut row = Vec::new();
+        for row_index in 0..week_count {
+            if let Some(contribution) = weekly_statuses[row_index].get(column_index) {
+                row.push(contribution);
+            }
+        }
+        kusa.push(row);
+    }
+    return kusa;
+}
+
+fn print_month(kusa: &Vec<Vec<&DailyStatus>>) {
+    let months = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+    ];
+    let mut month_line  = "".to_string();
+    for (index, daily_status) in kusa[0].iter().enumerate() {
+        if index == 0 {
+            let month = daily_status.get_month();
+            month_line += months[month - 1];
+            continue;
+        }
+
+        let month = daily_status.get_month();
+        let previous_month = kusa[0][index - 1].get_month();
+
+        if month != previous_month {
+            let whitespace_count = index * 2;
+            let current = month_line.len();
+            let require_space = whitespace_count - current;
+            if require_space > 0 {
+                let adjustment = " ".repeat(whitespace_count - current);
+                month_line += &adjustment;
+                month_line += months[month - 1];
+            }
+            else {
+                month_line.remove(3);
+                month_line += &" ".repeat(3 + require_space);
+                month_line += months[month - 1];
+            }
+        }
+
+    }
+    println!("{}", month_line);
+}
+
+fn print_gradation(kusa: &Vec<Vec<&DailyStatus>>) {
+    let start_point = (kusa[6].len()) * 2 - 18;
+    let colors = [
+        "#ebedf0", //Less
+        "#9be9a8",
+        "#40c463",
+        "#30a14e",
+        "#216e39",//More
+    ];
+    let whitespaces = " ".repeat(start_point);
+    print!("{}", whitespaces);
+    print!("Less ");
+    for color in colors {
+        let rgb = color.to_string().get_rgb();
+        print!("\x1b[38;2;{};{};{}m■\x1b[m ", rgb.r, rgb.g, rgb.b);
+    }
+    print!("More\n");
+}
+
+fn print_kusa(kusa: &Vec<Vec<&DailyStatus>>) {
+    for weekly_kusa in kusa {
+        for daily_kusa in weekly_kusa {
+            let rgb = daily_kusa.color.to_string().get_rgb();
+            print!("\x1b[38;2;{};{};{}m■\x1b[m ", rgb.r, rgb.g, rgb.b);
+        }
+        println!("");
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Command::parse();
+    let user_name = args.user_name;
+
+    let data = post_graphql_query(&user_name)?;
+    let (total_contributions, weekly_statuses) = get_github_contributions(data);
+    let kusa = transpose(&weekly_statuses);
+
+    println!("{} contributions in the last year", total_contributions);
+    print_month(&kusa);
+    print_kusa(&kusa);
+    print_gradation(&kusa);
+
+    Ok(())
+}
